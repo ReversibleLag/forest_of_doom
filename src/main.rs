79588164c21bdaba9fa7b3907117bbb0b0c873e2
ReversibleLag <kaisouth123@gmail.com>
2024-12-26 01:34:07 -0500

    added json file handling and test data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Pages {
+    pub pages: Vec<Page>,
+}
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Page {
+    pub number: i64,
+    pub text: String,
+    // pub actions: Actions,
+    pub next: i64,
+}
+
+// struct Player {}
+
+fn load_pages() {
+    let file_contents =
+        std::fs::read_to_string("src/pages.json").expect("Failed to read JSON file!!");
+    let pages: Pages = serde_json::from_str(&file_contents).expect("Failed to deserialize");
+    // let json:pages
+    println!("{:#?}", pages);
+    if let Some(first_page) = pages.pages.first() {
+        println!("First page number: {}", first_page.number);
+    } else {
+        println!("No pages found in the JSON file.");
+    }
+}
+
 fn main() {
+    load_pages();
     println!("Hello, world!");
 }
